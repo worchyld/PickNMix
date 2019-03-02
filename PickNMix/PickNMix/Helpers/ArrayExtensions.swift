2c33e6ec088ee83262f2ee44ebd1115673fc2ca5
AD <silentflute.ad@gmail.com>
2019-03-02 15:36:06 +0000

    Adding podfile, but this is ignored in the .gitignore.   Also adding array extensions and some temporary code to help with random generation

---
+++ PickNMix/PickNMix/Helpers/ArrayExtensions.swift
@@ -0,0 +1,34 @@
+// Array Extensions
+
+// Fisher-Yates (fast an uniform) shuffle
+// as per @url: http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift/24029847#24029847
+
+extension MutableCollection {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+
+        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let i = index(firstUnshuffled, offsetBy: d)
+            swapAt(firstUnshuffled, i)
+        }
+    }
+
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
+
+extension Collection where Indices.Iterator.Element == Index {
+
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Iterator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
--- PickNMix/PickNMix/ViewModels/PageViewModel.swift
+++ PickNMix/PickNMix/ViewModels/PageViewModel.swift
@@ -7,6 +7,76 @@
 //
 
 import Foundation
+import Gameplaykit
 
 struct PageViewModel {
+	private let industries : [Industry]
+	private let triggers : [Trigger]
+	private let businesses : [Business]
+
+	init(businesses: [Business], industries: [Industry], triggers: [Trigger]) {
+		self.industries = industries
+		self.businesses = businesses
+		self.triggers = triggers
+	}
+}
+
+extension PageViewModel {
+
+
+	// Early attempt at refactor
+	// Could this be done with an enum?
+	func getRandomObjectFrom(array: Array) -> nil {
+		guard array.count > 0 else {
+			return nil
+		}
+
+		let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: array)
+		guard let first = shuffled.first else {
+			return nil
+		}
+		return first
+	}
+
+
+	// Older versions
+
+	func getRandomIndustry() -> String? {
+		guard industries.count > 0 else {
+			return nil
+		}
+		
+		// Shuffle using Gameplaykit
+		let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: industries)
+		guard let first = shuffled.first else {
+			return nil
+		}
+		return first
+	}
+
+	func getRandomBusiness() -> String? {
+		guard businesses.count > 0 else {
+			return nil
+		}
+
+		// Shuffle using Gameplaykit
+		let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: businesses)
+		guard let first = shuffled.first else {
+			return nil
+		}
+		return first
+	}
+	func getRandomTrigger() -> String? {
+		guard triggers.count > 0 else {
+			return nil
+		}
+
+		// Shuffle using Gameplaykit
+		let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: triggers)
+		guard let first = shuffled.first else {
+			return nil
+		}
+		return first
+	}
+
 }
