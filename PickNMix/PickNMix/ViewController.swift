642bf79ed2602d6a97e9c67e9dad3ac71c68945c
AD <silentflute.ad@gmail.com>
2019-03-06 20:54:00 +0000

    updating code when the db is updated from api

--- PickNMix/PickNMix/ViewController.swift
+++ PickNMix/PickNMix/ViewController.swift
@@ -9,13 +9,73 @@
 import UIKit
 import SVProgressHUD
 import RealmSwift
+import GameplayKit
 
 class PageViewModel {
     private let entityModel : RootEntity
-    
+
     init(model: RootEntity) {
         self.entityModel = model
     }
+
+    /*
+    private struct Mix {
+        var industries: [String]
+        var businesses: [String]
+        var triggers: [String]
+
+        lazy var randomIndustry: String? = {
+            // Shuffle using Gameplaykit
+            let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: industries)
+            guard let first = shuffled.first else {
+                return nil
+            }
+            return first as? String
+        }()
+
+        lazy var randomBusinessModel: String? = {
+            // Shuffle using Gameplaykit
+            let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: businesses)
+            guard let first = shuffled.first else {
+                return nil
+            }
+            return first as? String
+        }()
+
+        lazy var randomTrigger: String? = {
+            // Shuffle using Gameplaykit
+            let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: triggers)
+            guard let first = shuffled.first else {
+                return nil
+            }
+            return first as? String
+        }()
+    }
+
+    func populate() {
+        guard let industries = entityModel.industries else {
+            return
+        }
+        guard let triggers = entityModel.triggers else {
+            return
+        }
+        guard let businessModels = entityModel.businessModels else {
+            return
+        }
+
+        for industry in industries {
+            self.mix?.industries.append(industry.name)
+        }
+
+        for trigger in triggers {
+            self.mix?.triggers.append(trigger.name)
+        }
+
+        for business in businessModels {
+            self.mix?.businesses.append(business.name)
+        }
+    }
+ */
 }
 
 extension PageViewModel : CustomStringConvertible {
@@ -38,17 +98,10 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     private var model: PageViewModel!
 
-    init(model: PageViewModel) {
-        self.model = model
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.model = PageViewModel(model: RootEntity())
 
         print (Realm.Configuration.defaultConfiguration.fileURL as Any)
 
@@ -82,7 +135,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             DispatchQueue.main.async {
                 SVProgressHUD.dismiss()
                 self.refreshCtrl.endRefreshing()
-                print(self.model.description)
+                print("Bounce back: \(self.model.description)")
             }
         }
     }
